6efce55621160ee76a65e5fe6151b4a7608794d5
edo <edo@gmail.com>
2024-11-25 00:53:08 +0700

    Try

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.sidebar.title("Menu")
+st.sidebar.write("Ini adalah konten sidebar")
+
+# Konten utama
+st.title("Aplikasi Streamlit")
+st.write("Ini adalah konten utama")
