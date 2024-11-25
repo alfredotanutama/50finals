9e0e14bed81630b6d3aae84fbda3558cf9465aad
edo <edo@gmail.com>
2024-11-26 00:15:27 +0700

    add pics

--- home.py
+++ home.py
@@ -1,9 +1,12 @@
 import streamlit as st # type: ignore
-
+from PIL import Image
+image = Image.open('NasiGoreng.jpg')
 st.set_page_config(
     page_title= "Byte2Bite",
     page_icon="🍴"
 )
 
-st.title("Bytee🍴Bite")
+st.title("Byte🍴Bite")
+st.subheader("Help you Byte the Bite")
+st.image(image,caption='Best Nasi Goreng in Town', width=600)
 st.sidebar.success("Select a page above.")
