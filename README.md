f063f8aa0930afde58c82e7772bfa68ff7db2e57
edo <edo@gmail.com>
2024-11-26 00:05:46 +0700

    streamlit try

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ cs50x final project
 First Run DB need:
   >  sqlite3 ingredients.db
   >  .read startup.sql
+
+First Run Main:
+  > streamlit run main.py
---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st # type: ignore
+
+st.set_page_config(
+    page_title= "Byte2Bite",
+    page_icon="🍴"
+)
+
+st.title("Bytee🍴Bite")
+st.sidebar.success("Select a page above.")
